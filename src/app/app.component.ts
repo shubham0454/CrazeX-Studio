0586516213dd03eb959afc047a904b2768c36596
shubham0454 <shubhamkshirsagar4045@gmail.com>
2024-12-27 18:52:06 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
+import { ServicesComponent } from "./components/services/services.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ HomeComponent, PortfolioComponent, ServicesComponent, AboutComponent, ContactComponent, FooterComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'crazex-project';
