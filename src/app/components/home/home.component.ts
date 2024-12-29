d4840472381553266c3d32d51cfe14da34260440
shubham0454 <shubhamkshirsagar4045@gmail.com>
2024-12-29 20:16:01 +0530

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  @ViewChild('videoBackground') videoBackground!: ElementRef;
 
+  ngOnInit() {
+    const videoElement = this.videoBackground.nativeElement;
 
+    // Using Intersection Observer to lazy load the video when it comes into view
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          videoElement.play(); // Play the video when the element comes into view
+        } else {
+          videoElement.pause(); // Pause the video when it goes out of view
+        }
+      });
+    });
+
+    observer.observe(videoElement);
+  }
 }
