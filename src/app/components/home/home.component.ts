a603195852d9f454c761b5bd3f2fdb142bee73b3
shubham0454 <shubhamkshirsagar4045@gmail.com>
2024-12-29 19:06:16 +0530

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,18 +7,6 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  @ViewChild('backgroundVideo') backgroundVideo: ElementRef<HTMLVideoElement> | undefined;
 
-  ngOnInit(): void {
-    if (this.backgroundVideo) {
-      const video = this.backgroundVideo.nativeElement;
-      // Ensure the video is muted and autoplay
-      video.muted = true; // Explicitly mute the video
-      video.autoplay = true; // Explicitly enable autoplay
-      video.loop = true; // Enable loop for the video
-      video.play().catch((err) => {
-        console.error('Autoplay failed:', err);
-      });
-    }
-  }
+
 }
