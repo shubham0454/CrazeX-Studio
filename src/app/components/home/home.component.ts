0938832cae5bc89f5544bc22f74299bf85e76b3a
shubham0454 <shubhamkshirsagar4045@gmail.com>
2024-12-30 18:04:03 +0530

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,41 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent  {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    this.setActiveVideo();
+  }
 
+  setActiveVideo() {
+    const videos = document.querySelectorAll('.carousel-item video');
+    const carousel = document.querySelector('#heroCarousel') as HTMLElement;
+    
+
+    carousel.addEventListener('slid.bs.carousel', () => {
+      videos.forEach((video: Element) => {
+        const videoElement = video as HTMLVideoElement; // Cast to HTMLVideoElement
+        if (videoElement.closest('.carousel-item')?.classList.contains('active')) {
+          videoElement.play();
+          setTimeout(() => {
+            videoElement.pause();
+          }, 40000);
+        } else {
+          videoElement.pause();
+        }
+      });
+    });
+
+
+    videos.forEach((video: Element) => {
+      const videoElement = video as HTMLVideoElement; // Cast to HTMLVideoElement
+      if (videoElement.closest('.carousel-item')?.classList.contains('active')) {
+        videoElement.play();
+        setTimeout(() => {
+          videoElement.pause();
+        }, 40000);
+      } else {
+        videoElement.pause();
+      }
+    });
+  }
 }
